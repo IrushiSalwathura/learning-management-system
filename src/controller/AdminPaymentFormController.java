1987c651fdbe1ee6f848e754efc0bd9625373d51
IrushiSalwathura <irushitharindi@gmail.com>
2020-08-04 21:51:41 +0530

    Add AdminPaymentFormController

---
+++ src/controller/AdminPaymentFormController.java
@@ -0,0 +1,26 @@
+package controller;
+
+import javafx.event.ActionEvent;
+
+public class AdminPaymentFormController {
+    public void btnCourses_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnModules_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnAccount_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnView_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnAdd_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnDelete_OnActions(ActionEvent actionEvent) {
+    }
+
+    public void btnDashboard_OnAction(ActionEvent actionEvent) {
+    }
+}
--- src/controller/StudentPaymentFormController.java
+++ src/controller/StudentPaymentFormController.java
@@ -1,4 +1,17 @@
 package controller;
 
+import javafx.event.ActionEvent;
+
 public class StudentPaymentFormController {
+    public void btnDashboard_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnCourses_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnModules_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnAccount_OnAction(ActionEvent actionEvent) {
+    }
 }
