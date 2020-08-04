3ac0c31111b9a6f522c025d8c3a83747d5343feb
IrushiSalwathura <irushitharindi@gmail.com>
2020-08-04 19:43:51 +0530

    Add AdminModulesForm and close #23

---
+++ src/controller/AdminModuleFormController.java
@@ -0,0 +1,17 @@
+package controller;
+
+import javafx.event.ActionEvent;
+
+public class AdminModuleFormController {
+    public void btnDashboard_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnCourses_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnModules_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnAccount_OnAction(ActionEvent actionEvent) {
+    }
+}
