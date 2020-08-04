8f8280cb9a81065ad368f7c76c66ad3a39cd2085
Chinthani <chinthani96@gmail.com>
2020-08-04 14:38:25 +0530

    Add AppInitializer, MainForm of Student and fix #1

---
+++ src/controller/MainFormController.java
@@ -0,0 +1,18 @@
+package controller;
+
+import javafx.event.ActionEvent;
+
+public class MainFormController {
+    public void btnDashboard_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnCourses_OnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void btnModules_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnAccount_OnAction(ActionEvent actionEvent) {
+    }
+}
