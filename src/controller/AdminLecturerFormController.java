72956feb4818eee6f0b494989a7601584d471fab
IrushiSalwathura <irushitharindi@gmail.com>
2020-08-04 18:58:58 +0530

    Add AdminLecturerForm and close #19

---
+++ src/controller/AdminLecturerFormController.java
@@ -0,0 +1,26 @@
+package controller;
+
+import javafx.event.ActionEvent;
+
+public class AdminLecturerFormController {
+    public void btnDashboard_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnCourses_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnModules_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnAccount_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnAdd_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnDelete_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSave_OnAction(ActionEvent actionEvent) {
+    }
+}
