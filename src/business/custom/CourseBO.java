33eb56d3a0425cdcae6f9486dbe1dd074242e358
IrushiSalwathura <irushitharindi@gmail.com>
2020-08-06 14:17:12 +0530

    Update CourseBO

--- src/business/custom/CourseBO.java
+++ src/business/custom/CourseBO.java
@@ -1,10 +1,11 @@
 package business.custom;
 
+import business.SuperBO;
 import util.CourseTM;
 
 import java.util.List;
 
-public interface CourseBO {
+public interface CourseBO extends SuperBO {
     List<CourseTM> getAllCourses() throws Exception;
     boolean saveCourseDetails(String id, String title, String type, double duration) throws Exception;
     boolean deleteCourse(String id) throws Exception;
