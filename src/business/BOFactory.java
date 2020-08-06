00311194d07284d95c8ff22c1d16513c64b97c67
Chinthani <chinthani96@gmail.com>
2020-08-06 12:36:21 +0530

    Add implementation to BOFactory class and Update BOType

--- src/business/BOFactory.java
+++ src/business/BOFactory.java
@@ -1,4 +1,29 @@
 package business;
 
+import business.custom.impl.ContentBOImpl;
+import business.custom.impl.ModuleBOImpl;
+import business.custom.impl.StudentBOImpl;
+
 public class BOFactory {
+    private static BOFactory boFactory;
+
+    private BOFactory() {
+    }
+
+    public static BOFactory getInstance(){
+        return (boFactory==null)?boFactory=new BOFactory():boFactory;
+    }
+
+    public <T extends SuperBO> T getBO(BOType boType){
+        switch(boType){
+            case STUDENT:
+                return (T) new StudentBOImpl();
+            case MODULE:
+                return (T) new ModuleBOImpl();
+            case CONTENT:
+                return (T) new ContentBOImpl();
+                default:
+                return null;
+        }
+    }
 }
