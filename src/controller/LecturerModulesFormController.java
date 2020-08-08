5d8afbdc5514b6689e842eca63bd5fef180a9e45
Chinthani <chinthani96@gmail.com>
2020-08-08 12:02:30 +0530

    Add methods to load Courses, Faculties and modules to combo boxes

--- src/controller/LecturerModulesFormController.java
+++ src/controller/LecturerModulesFormController.java
@@ -1,16 +1,41 @@
 package controller;
 
+import business.custom.LecturerBO;
+import business.custom.ModuleBO;
+import business.custom.impl.LecturerBOImpl;
+import business.custom.impl.ModuleBOImpl;
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXComboBox;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ListView;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import util.CourseTM;
+import util.FacultyTM;
+import util.ModuleTM;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 
 public class LecturerModulesFormController {
     public AnchorPane root;
+    public JFXButton btnDashboard;
+    public JFXComboBox cmbCourses;
+    public JFXComboBox cmbFaculty;
+    public ListView lstModules;
+
+    public void initialize() throws Exception {
+        loadAllFacultiesOfLecturer("L001");
+        loadAllCoursesOfLecturerInFaculty("L001","F001");
+        loadAllCourseModules("C001");
+    }
 
     public void btnDashboard_OnAction(ActionEvent actionEvent) throws IOException {
         Parent root = FXMLLoader.load(this.getClass().getResource(""));
@@ -43,4 +68,29 @@ public class LecturerModulesFormController {
         mainStage.setScene(mainScene);
         mainStage.centerOnScreen();
     }
+
+    public void loadAllFacultiesOfLecturer(String lecturerId) throws Exception {
+        cmbFaculty.getItems().clear();
+        LecturerBO lecturerBO = new LecturerBOImpl();
+        List<FacultyTM> faculties = lecturerBO.getLecturerFaculties(lecturerId);
+        ObservableList<FacultyTM> facultyTMS = FXCollections.observableArrayList(faculties);
+        cmbFaculty.setItems(facultyTMS);
+    }
+
+    public void loadAllCoursesOfLecturerInFaculty(String lecturerId, String facultyId) throws Exception {
+        cmbCourses.getItems().clear();
+        LecturerBO lecturerBO = new LecturerBOImpl();
+        List<CourseTM> lecturerFacultyCourses = lecturerBO.getLecturerFacultyCourses(lecturerId, facultyId);
+        ObservableList<CourseTM> courseTMS = FXCollections.observableArrayList(lecturerFacultyCourses);
+        cmbCourses.setItems(courseTMS);
+    }
+
+    public void loadAllCourseModules(String courseId) throws Exception {
+        lstModules.getItems().clear();
+        ModuleBO moduleBO= new ModuleBOImpl();
+        List<ModuleTM> courseModules = moduleBO.getCourseModules(courseId);
+        ObservableList<ModuleTM> moduleTMS = FXCollections.observableArrayList(courseModules);
+        lstModules.setItems(moduleTMS);
+    }
+
 }
