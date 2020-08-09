0652e06339fca1b2af1a845f705985bcdc6b658f
Chinthani <chinthani96@gmail.com>
2020-08-09 13:24:40 +0530

    Update Lecturer entity

--- src/entity/Lecturer.java
+++ src/entity/Lecturer.java
@@ -1,7 +1,5 @@
 package entity;
 
-import java.io.Serializable;
-
 public class Lecturer implements SuperEntity {
 
     private String id;
@@ -9,24 +7,22 @@ public class Lecturer implements SuperEntity {
     private String name;
     private String address;
     private String contact;
-    private String username;
-    private String password;
     private String nic;
     private String email;
+    private String userId;
 
     public Lecturer() {
     }
 
-    public Lecturer(String id, String courseId, String name, String address, String contact, String username, String password, String nic, String email) {
+    public Lecturer(String id, String courseId, String name, String address, String contact, String nic, String email, String userId) {
         this.id = id;
         this.courseId = courseId;
         this.name = name;
         this.address = address;
         this.contact = contact;
-        this.username = username;
-        this.password = password;
         this.nic = nic;
         this.email = email;
+        this.userId = userId;
     }
 
     public String getId() {
@@ -69,22 +65,6 @@ public class Lecturer implements SuperEntity {
         this.contact = contact;
     }
 
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
     public String getNic() {
         return nic;
     }
@@ -101,6 +81,14 @@ public class Lecturer implements SuperEntity {
         this.email = email;
     }
 
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
     @Override
     public String toString() {
         return "Lecturer{" +
@@ -109,10 +97,9 @@ public class Lecturer implements SuperEntity {
                 ", name='" + name + '\'' +
                 ", address='" + address + '\'' +
                 ", contact='" + contact + '\'' +
-                ", username='" + username + '\'' +
-                ", password='" + password + '\'' +
                 ", nic='" + nic + '\'' +
                 ", email='" + email + '\'' +
+                ", userId='" + userId + '\'' +
                 '}';
     }
 }
